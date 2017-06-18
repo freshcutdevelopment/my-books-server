90dd0f7c343771fc2a45586e58a54cc3a3bb6bc0
Sean Hunter <sean.hunter@onset.com.au>
2017-06-18 10:15:59 +1000

    Added CORS support

--- server.js
+++ server.js
@@ -106,6 +106,11 @@ router.route('/shelves')
             });
        });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/api', router); //All API routes should sit under the /api route prefix.
 
